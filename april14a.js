ce7667044e9eeb3db115dd30684867748644015a
Peter Sigurdson <peter@petersigurdson.net>
2021-04-14 14:47:23 -0400

    Update april14a.js

--- april14a.js
+++ april14a.js
@@ -1,5 +1,5 @@
 // APRIL14A.JS
-counter version
+counter version: working version of code
 
     // APRIL14A.JS
 
@@ -21,6 +21,37 @@ counter version
         for ( ; counter < 2 ; counter++){
             console.log(counter);
             globalCounter++;
+        }
+    }
+
+    var L = lemon();
+    // console.log(L);
+    console.log("fini");
+
+
+
+Logical Flag Version : Not working yet:
+
+ // APRIL14A.JS
+
+    var LogicalFlag = true;
+    var occuranceCounter = 0;
+
+    function lemon(){
+        if (occuranceCounter >8) LogicalFlag = false;
+        console.log("trace msg A: ", occuranceCounter)
+    
+        while (LogicalFlag){
+            orange();
+        } 
+        console.log("trace msg B: Done and Finished!")
+    }
+
+    function orange(){
+        var counter = 0;
+        for ( ; counter < 2 ; counter++){
+            occuranceCounter++;
+            console.log(counter);
         }
     }
 
