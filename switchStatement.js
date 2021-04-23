5e862ae6b52db4dcc15d9fac156b5eaeb9871da8
Peter Sigurdson <peter@petersigurdson.net>
2021-04-23 14:59:33 -0400

    Create switchStatement.js

---
+++ switchStatement.js
@@ -0,0 +1,16 @@
+switch(variable/expression) {
+    case value1:  
+        // body of case 1
+        break;
+
+    case value2:  
+        // body of case 2
+        break;
+
+    case valueN:
+        // body of case N
+        break;
+
+    default:
+        // body of default
+}
